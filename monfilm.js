736c849c7d4869495f5f0c06017ee0ed1b9b95d9
MarcGregoireBalme <47778722+MarcGregoireBalme@users.noreply.github.com>
2019-03-11 10:04:08 +0100

    Add files via upload

---
+++ monfilm.js
@@ -0,0 +1,7 @@
+// variables
+const myFilm = 'Lord of War';
+const productor = 'Andrew Nicccol';
+const date ="2005"
+const message = myFilm + ", réalisé par " + productor + " est sortie en " + date
+//pop up
+alert(message);
